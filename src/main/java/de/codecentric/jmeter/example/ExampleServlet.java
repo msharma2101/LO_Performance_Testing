90038691177f69a0d277e0bca6038125cf179c26
msharma2101 <msharma@usgbc.org>
2020-04-27 15:01:57 -0400

    new files

---
+++ src/main/java/de/codecentric/jmeter/example/ExampleServlet.java
@@ -0,0 +1,32 @@
+/*package de.codecentric.jmeter.example;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.util.Random;
+
+
+public class ExampleServlet extends GenericServlet {
+    private Random random = new Random();
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		sleep(100);
+		res.getOutputStream().print("Hello ...");
+		res.getOutputStream().flush();
+		sleep(100);
+		res.getOutputStream().print("... World");
+    }
+
+	private void sleep(int interval) {
+		try {
+			// Wait a random time between 0.5*interval and 1.5*interval msec
+			Thread.sleep(random.nextInt(interval) + interval/2);
+		} catch (InterruptedException e) {
+			// Don't care if we've been interrupted
+		}
+	}
+}
+*/
